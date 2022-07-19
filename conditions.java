877b9db9e592da3279d644ec948041e67b23b9a6
VineeshaReddy92 <Gvineeshareddy@gmail.com>
2022-07-19 14:25:06 +0530

    updated Conditions

--- conditions.java
+++ conditions.java
@@ -1,2 +1,3 @@
 This is the First Class of Git hub Tool.
 We are going to learn How to create repositaries and how to move the files one area to another area.
+and How to push the code to Remote Repository.
