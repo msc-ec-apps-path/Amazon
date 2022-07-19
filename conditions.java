daa850d6059fe82203ce6978d0969542bd5ddeb3
VineeshaReddy92 <Gvineeshareddy@gmail.com>
2022-07-19 14:47:38 +0530

    added some test file

--- conditions.java
+++ conditions.java
@@ -1,3 +1,4 @@
 This is the First Class of Git hub Tool.
 We are going to learn How to create repositaries and how to move the files one area to another area.
 and How to push the code to Remote Repository.
+first we have to create the files it will be local repository, then we have to move from working area to staging area and we have to ull the code to Remote Repository.
